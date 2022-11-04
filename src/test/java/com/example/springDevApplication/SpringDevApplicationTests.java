ee231208e80e878bbea36536c8bb0fdf0739e2ed
thusiyanth <31480624+thusiyanth@users.noreply.github.com>
2022-11-04 18:38:30 +0530

    Initial commit

---
+++ src/test/java/com/example/springDevApplication/SpringDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDevApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
